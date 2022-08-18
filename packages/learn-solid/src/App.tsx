87b3e251fa068f3da6ab8143d51a52b60aba5a7a
okbug <1583978087@qq.com>
2022-08-18 14:19:42 +0800

    feat: add solidjs and sveltejs project

---
+++ packages/learn-solid/src/App.tsx
@@ -0,0 +1,14 @@
+import type { Component } from 'solid-js';
+
+import logo from './logo.svg';
+import styles from './App.module.css';
+
+const App: Component = () => {
+  return (
+    <div class={styles.App}>
+     <h1>SolidJS</h1>
+    </div>
+  );
+};
+
+export default App;
