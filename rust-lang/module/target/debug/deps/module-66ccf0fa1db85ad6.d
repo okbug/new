ba9d783549353bd9f36932e85f394eeda3728ee8
okbug <1583978087@qq.com>
2022-09-03 13:25:52 +0800

    go

---
+++ rust-lang/module/target/debug/deps/module-66ccf0fa1db85ad6.d
@@ -0,0 +1,10 @@
+/Users/ajun/Desktop/study/new/rust-lang/module/target/debug/deps/module-66ccf0fa1db85ad6.rmeta: src/main.rs src/router.rs src/request/mod.rs src/request/core.rs
+
+/Users/ajun/Desktop/study/new/rust-lang/module/target/debug/deps/module-66ccf0fa1db85ad6.d: src/main.rs src/router.rs src/request/mod.rs src/request/core.rs
+
+src/main.rs:
+src/router.rs:
+src/request/mod.rs:
+src/request/core.rs:
+
+# env-dep:CLIPPY_ARGS=
